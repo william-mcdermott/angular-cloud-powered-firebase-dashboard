6e4efe3c6ab5703a6ac0fa37f06bfb6adfd74555
William McDermott <williammcdermott@Ubbas-MBP.lan>
2023-03-26 16:31:33 -0500

    Get admin page to list users working

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { UserProfile } from '../core/user-profile.model';
 
 @Component({
   selector: 'app-users',
@@ -6,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
-
-  constructor() { }
+  private usersCollection: AngularFirestoreCollection<UserProfile>;
+  public users: Observable<UserProfile[]>;
+  constructor(private afs: AngularFirestore) {
+    this.usersCollection = afs.collection<UserProfile>('users');
+    this.users = this.usersCollection.valueChanges();
+  }
 
   ngOnInit() {
   }
