8188e097a9c6235cfae25299535075169552bc78
William McDermott <williammcdermott@Ubbas-MBP.lan>
2023-03-25 21:03:24 -0500

    require permissions in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,29 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { map } from 'rxjs/operators';
+import { AngularFireAuthGuard, redirectUnauthorizedTo } from '@angular/fire/auth-guard'
 
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo([''])
+const redirectLoggedInToProfile = () =>
+  map(user => user ? ['profile', (user as any).uid]: true);
+
+const onlyAllowSelf = next =>
+  map(
+    user => (!!user && next.params.id === (user as any).uid || [''])
+  );
 const routes: Routes = [
   {
     path: '',
-    component: LoginComponent
+    component: LoginComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: redirectLoggedInToProfile }
   },
   {
     path: 'profile/:id',
-    component: ProfileComponent
+    component: ProfileComponent,
+    canActivate: [AngularFireAuthGuard],
+    data: { authGuardPipe: onlyAllowSelf }
   }
 ];
 
